8cf1b024dda280ca3baa6adf1deed585de84dbb3
Ampser <102514508+Ampser@users.noreply.github.com>
2024-08-07 02:06:36 +0700

    Add files via upload

---
+++ lab06/01.py
@@ -0,0 +1,7 @@
+n =int((input()))
+for i in range (1,n+1):
+    for j in range(1,n+1):
+        if(i==1 or i==n or j == 1 or j==n or j==i or j==n-i+1 ):
+            print(".",end="")
+        else: print(" ",end="")
+    print()
